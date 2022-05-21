a3605a932a17ef56ba5825f62c2c896cfb9a34b9
Carlos-Lemus195 <202000195@udv.edu.gt>
2022-05-20 18:34:19 -0600

    Cambiada la ubicacion de los componentes para tener mas orden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegistroEmpleadosComponent } from './components/registro_empleados/registro-empleados/registro-empleados.component';
+import { RegistroEmpleadosComponent } from './components/empleados/registro-empleados/registro-empleados.component';
 import { AppComponent } from './app.component';
-import { ListarSucursalComponent } from './components/listar-sucursal/listar-sucursal.component';
-import { SucursalComponent } from './components/sucursal/sucursal.component';
+import { ListarSucursalComponent } from './components/sucursal/listar-sucursal/listar-sucursal.component';
+import { SucursalComponent } from './components/sucursal/sucursal/sucursal.component';
 import { RutinaComponent } from './components/rutina/rutina/rutina.component';
 import { LoginComponent } from './components/login/login.component';
 import { MasterComponent } from './components/roles/master/master.component';
 import { InstructorComponent } from './components/roles/instructor/instructor.component';
 import { AdminComponent } from './components/roles/admin/admin.component';
 import { PagoPlataformaComponent } from './components/pago-plataforma/pago-plataforma.component';
-import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
-import { NuevarutinaComponent } from './components/nuevarutina/nuevarutina/nuevarutina.component';
-import { RegistroClientesComponent } from './components/registro-clientes/registro-clientes.component';
-import { ListarClientesComponent } from './components/listar-clientes/listar-clientes.component';
+import { ListaEmpleadosComponent } from './components/empleados/lista-empleados/lista-empleados.component';
+import { NuevarutinaComponent } from './components/rutina/nuevarutina/nuevarutina.component';
+import { RegistroClientesComponent } from './components/clientes/registro-clientes/registro-clientes.component';
+import { ListarClientesComponent } from './components/clientes/listar-clientes/listar-clientes.component';
 import { CreardietaComponent } from './components/dietas/creardieta/creardieta.component';
 import { DietasComponent } from './components/dietas/dietas/dietas.component';
 
